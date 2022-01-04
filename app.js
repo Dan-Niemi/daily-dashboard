5d586f9225b57a3bc49a4b4e72123a10fe9b3f18
Dan Niemi <niemdani@gmail.com>
2022-01-04 11:37:39 -0600

    adding note Class

--- app.js
+++ app.js
@@ -1,66 +1,23 @@
+import Note from "./class/Note.js";
 
-document.addEventListener('dblclick', createNote)
-document.addEventListener('input', resizeNote)
+let noteboard = document.querySelector('.noteboard');
+noteboard.addEventListener('dblclick', createNote)
 
 function createNote(e) {
-  if (e.target.classList.contains('noteboard')) {
-    let newNote = document.createElement('div');
-    let rect = e.target.getBoundingClientRect();
-    newNote.className = 'note';
-    newNote.style.left = e.clientX - rect.left + 'px'
-    newNote.style.top = e.clientY - rect.top + 'px'
-    e.target.appendChild(newNote)
+  let rect = noteboard.getBoundingClientRect();
+  let posX = e.clientX - rect.left
+  let posY = e.clientY - rect.top
+  let newNote = new Note(posX, posY)
+  noteboard.appendChild(newNote)
+}
 
-    let grabArea = document.createElement('div');
-    grabArea.className = 'note__grab-area'
-    grabArea.addEventListener('mousedown', dragNote(newNote, grabArea))
-    newNote.appendChild(grabArea)
-
-    let textArea = document.createElement('textarea');
-    textArea.className = 'note__text-area'
-    textArea.spellcheck = false
-    newNote.appendChild(textArea)
-    textArea.focus()
-
+function getNotesFromLocalStorage() {
+  for (let i = 0; i < localStorage.length; i++) {
+    let id = localStorage.key(i)
+    let json = JSON.parse(localStorage.getItem(id))
+    let obj = { id, ...json }
+    noteboard.appendChild(new Note(obj.posX, obj.posY,obj.id, obj.text, obj.width, obj.height))
   }
 }
 
-function resizeNote(e) {
-  e.target.style.height = Math.max(e.target.offsetHeight, e.target.scrollHeight) + 'px'
-}
-
-
-function dragNote(note, grabArea) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  grabArea.onmousedown = dragMouseDown;
-
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // get the mouse cursor position at startup:
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    // calculate the new cursor position:
-    pos1 = pos3 - e.clientX;
-    pos2 = pos4 - e.clientY;
-    pos3 = e.clientX;
-    pos4 = e.clientY;
-    // set the element's new position:
-    note.style.top = (note.offsetTop - pos2) + "px";
-    note.style.left = (note.offsetLeft - pos1) + "px";
-  }
-
-  function closeDragElement() {
-    // stop moving when mouse button is released:
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
-}
+getNotesFromLocalStorage()
---
+++ class/Note.js
@@ -0,0 +1,82 @@
+export default class Note {
+  constructor(posX, posY,id, text, width, height,) {
+    const DEFAULT_WIDTH = 300
+    const DEFAULT_HEIGHT = 150
+
+    this.id = id || new Date().valueOf();
+    this.text = text || '';
+    this.posX = posX;
+    this.posY = posY;
+    this.width = width || DEFAULT_WIDTH;
+    this.height = height || DEFAULT_HEIGHT;
+    this.init()
+    this.setLocalStorage()
+    return this.note
+  }
+  init() {
+    this.note = document.createElement('div')
+    this.note.className = 'note'
+    this.note.setAttribute('data-id', this.id)
+    this.note.style.left = this.posX + 'px'
+    this.note.style.top = this.posY + 'px'
+
+    this.noteGrab = document.createElement('div')
+    this.noteGrab.setAttribute('class', 'note__grab-area')
+
+    this.noteText = document.createElement('textarea')
+    this.noteText.className = 'note__text-area'
+    this.noteText.spellcheck = false
+    this.noteText.style.width = this.width + 'px'
+    this.noteText.style.height = this.height + 'px'
+    this.noteText.textContent = this.text
+
+    this.note.appendChild(this.noteGrab)
+    this.note.appendChild(this.noteText)
+
+    this.noteGrab.addEventListener('mousedown', this.drag.bind(this))
+    this.noteText.addEventListener('input', this.updateText.bind(this))
+    this.noteText.addEventListener('blur', this.setLocalStorage.bind(this))
+    this.noteText.addEventListener('mouseup', this.setLocalStorage.bind(this))
+  }
+
+  updateText() {
+    this.text = this.noteText.value
+    this.autoResize()
+    this.setLocalStorage()
+  }
+
+  drag(e) {
+    let moveX = 0, moveY = 0, mouseX = e.clientX, mouseY = e.clientY
+    e = e || window.event;
+    e.preventDefault()
+    document.onmouseup = dragStop.bind(this);
+    document.onmousemove = dragMove.bind(this);
+
+    function dragMove(e) {
+      e = e || window.event;
+      e.preventDefault();
+      moveX = mouseX - e.clientX
+      moveY = mouseY - e.clientY
+      mouseX = e.clientX
+      mouseY = e.clientY
+
+      this.posX = this.note.offsetLeft - moveX
+      this.posY = this.note.offsetTop - moveY
+      this.note.style.left = this.posX + 'px'
+      this.note.style.top = this.posY + 'px'
+    }
+    function dragStop() {
+      this.setLocalStorage()
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
+
+  }
+  autoResize() {
+    this.noteText.style.height = Math.max(this.noteText.offsetHeight, this.noteText.scrollHeight) + 'px'
+  }
+  setLocalStorage() {
+    let obj = {posX: this.posX, posY: this.posY,text: this.text, width: parseInt(this.noteText.style.width),height: parseInt(this.noteText.style.height)}
+    localStorage.setItem(this.id, JSON.stringify(obj))
+  }
+}
